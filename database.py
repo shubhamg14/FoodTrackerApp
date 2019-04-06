35e292f91f57530ff6545e34de74d4a2e8a38ae7
Shubham Gupta <shubhamg14@gmail.com>
2019-04-06 02:33:23 -0400

    Firt Commit - All code moved

---
+++ database.py
@@ -0,0 +1,12 @@
+from flask import g
+import sqlite3
+
+def connect_db():
+    sql = sqlite3.connect('D://Projects/Python/Food-Tracker/food_log.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3'):
+        g.sqlite_db = connect_db()
+        return g.sqlite_db
